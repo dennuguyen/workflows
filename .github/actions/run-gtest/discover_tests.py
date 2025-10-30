3084f29ec46c99a9e068fa200180dfd6dde1fa88
dennuguyen <dennuguyen@gmail.com>
2025-10-30 14:06:11 +1100

    Modify discover_tests.py to not use clang

--- .github/actions/run-gtest/discover_tests.py
+++ .github/actions/run-gtest/discover_tests.py
@@ -1,44 +1,9 @@
 import re
 import sys
 
-from clang.cindex import CursorKind, Index, TranslationUnit
 from pathlib import Path
 from pydantic import BaseModel, field_validator
 from typing import Optional
-
-def discover_testcases(test_file: str) -> list[dict]:
-    index = Index.create()
-    tu = index.parse(test_file, options=TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD)
-    code = Path(test_file).read_text().splitlines()
-    testcases = []
-
-    # Traverse the AST.
-    for cursor in tu.cursor.walk_preorder():
-        try:
-            if cursor.kind == CursorKind.MACRO_INSTANTIATION and cursor.spelling.startswith("TEST"):
-                line = cursor.location.line
-                args = extract_test_macro_args(code[line - 1]) # -1 to account for 0 index.
-                metadata = extract_test_metadata(code, line)
-                testcases.append({
-                    "test_file": test_file,
-                    "test_runner": "./" + Path(test_file).stem + ".out",
-                    "id": args[0] + "." + args[1],
-                    **metadata,
-                })
-        except ValueError as e:
-            continue
-    return testcases
-
-def extract_test_macro_args(code: str) -> tuple:
-    """
-    Gets the test suite and case names from the TEST macro.
-    """
-    m = re.match(r"(TEST|TEST_F)\s*\(\s*(\w+)\s*,\s*(\w+)\s*\)", code)
-    if m:
-        testsuite_name = m.group(2)
-        testcase_name = m.group(3)
-        return testsuite_name, testcase_name
-    return None, None
 
 class TestMetadata(BaseModel):
     name: Optional[str] = None
@@ -66,19 +31,45 @@ class TestMetadata(BaseModel):
             return v.encode("utf-8").decode("unicode_escape")
         return v
 
-def extract_test_metadata(code: list[str], line: int) -> dict:
+def extract_test_metadata(code: list[str], row: int) -> dict:
     """
     Scans previous lines from the given line for testcase metadata.
     """
-    line -= 1
+    row -= 1
     metadata = {}
-    while line >= 0 and code[line - 1].startswith("//@"):
-        m = re.match(r"\/\/@(\w+)\s*(.*)", code[line - 1])
+    while row >= 0 and code[row].startswith("//@"):
+        m = re.match(r"\/\/@(\w+)\s*(.*)", code[row])
         if m:
             key = m.group(1)
             value = m.group(2) or True
             metadata[key] = value
-        line -= 1
+        row -= 1
     return TestMetadata(**metadata).model_dump(exclude_none=True)
 
-discover_testcases(sys.argv[0])
+def extract_test_name(code: str) -> tuple:
+    """
+    Gets the test suite and case names from the TEST macro.
+    """
+    m = re.match(r"(TEST|TEST_F)\s*\(\s*(\w+)\s*,\s*(\w+)\s*\)", code)
+    if m:
+        testsuite_name = m.group(2)
+        testcase_name = m.group(3)
+        return testsuite_name, testcase_name
+    return None, None
+
+def discover_testcases(test_file: str) -> list[dict]:
+    code = Path(test_file).read_text().splitlines()
+    testcases = []
+    for row, line in enumerate(code):
+        if line.startswith("TEST"):
+            args = extract_test_name(code[row])
+            metadata = extract_test_metadata(code, row)
+            testcases.append({
+                "test_file": test_file,
+                "test_runner": "./" + Path(test_file).stem + ".out",
+                "id": args[0] + "." + args[1],
+                **metadata,
+            })
+    return testcases
+
+print(discover_testcases(sys.argv[1]))
