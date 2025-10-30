3ae21410d87250683661241e8cfbc0e51eb75067
dennuguyen <dennuguyen@gmail.com>
2025-10-30 14:05:37 +1100

    Add test metadata

--- test_stage1.cpp
+++ test_stage1.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
 #include "hello.hpp"
 
+//@name Hello world test
+//@min_score 0
+//@max_score 1
+//@hidden true
 TEST(HelloWorldTest, BasicAssertions) {
     // Expect two strings to be equal.
     EXPECT_EQ(HelloWorld(), "Hello World!");
