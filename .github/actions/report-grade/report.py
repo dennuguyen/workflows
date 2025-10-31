3611efc9831375e2933b76c6a79ae5594230daac
dennuguyen <dennuguyen@gmail.com>
2025-10-31 14:29:46 +1100

    Dump json and use core.notice

--- .github/actions/report-grade/report.py
+++ .github/actions/report-grade/report.py
@@ -1,7 +1,9 @@
+import json
+
 from actions_toolkit import core
 
 results = core.get_multiline_input("results")
 
 for result in results:
     with open(result, "r") as f:
-        core.info(f)
+        core.notice(json.dumps(f))
