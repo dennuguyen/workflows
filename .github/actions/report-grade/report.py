5f53e3b102704c024b41c2490d87aaa1f79728a5
dennuguyen <dennuguyen@gmail.com>
2025-10-31 14:02:23 +1100

    Pass file name instead of contents between Actions

--- .github/actions/report-grade/report.py
+++ .github/actions/report-grade/report.py
@@ -3,4 +3,5 @@ from actions_toolkit import core
 results = core.get_multiline_input("results")
 
 for result in results:
-    core.info(str(result))
+    with open(result, "r") as f:
+        core.info(f)
