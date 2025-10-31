ea6563de8eb73819d1439919930f5f5c34abdfb3
dennuguyen <dennuguyen@gmail.com>
2025-10-31 17:26:35 +1100

    Print debugging statement in report-grade to check input

--- .github/actions/report-grade/report.py
+++ .github/actions/report-grade/report.py
@@ -4,6 +4,8 @@ from actions_toolkit import core
 
 results = core.get_multiline_input("results")
 
+print("DEBUGGING", results)
+
 for result in results:
     with open(result, "r") as f:
         res = json.load(f)
