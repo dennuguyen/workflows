c1e6f47fb9f017467d6533834d26335bf845e4ce
dennuguyen <dennuguyen@gmail.com>
2025-10-31 17:30:02 +1100

    Pass results as one string

--- .github/actions/report-grade/report.py
+++ .github/actions/report-grade/report.py
@@ -6,6 +6,10 @@ results = core.get_input("results")
 
 print("DEBUGGING", results)
 
+results2 = core.get_multiline_input("results")
+print("DEBUGGING", results2)
+
+
 for result in results:
     with open(result, "r") as f:
         res = json.load(f)
