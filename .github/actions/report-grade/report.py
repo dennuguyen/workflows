d6b2a625aa032b3fb9eb96085d65dcfac90e3af9
dennuguyen <dennuguyen@gmail.com>
2025-10-31 18:00:12 +1100

    Use comma-separated string for results input in report-grade

--- .github/actions/report-grade/report.py
+++ .github/actions/report-grade/report.py
@@ -4,13 +4,9 @@ import os
 from actions_toolkit import core
 
 results = os.getenv("INPUT_RESULTS", "")
-
-# normalize to list
-# if isinstance(results, str):
-#     results = [r.strip() for r in results.split(",") if r.strip()]
+results = [r.strip() for r in results.split(",")]
 
 print("DEBUGGING", results)
-
 
 for result in results:
     with open(result, "r") as f:
